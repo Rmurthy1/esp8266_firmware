029cdc748765e98654d5a6d271b08f69cb9d5476
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-02-05 23:52:21 -0800

    Update esp8266_firmware.ino
    added wifi,f status led, and a cooler blink method

--- esp8266_firmware.ino
+++ esp8266_firmware.ino
@@ -1,9 +1,17 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#include "secureConfig.h"
+
+ESP8266WiFiMulti WiFiMulti;
+
 
 String message = "";
 bool messageReady = false;
 bool lightBlink = true;
-
-#include <SoftwareSerial.h>
+bool lightOn = true;
 
 const byte rxPin = 14; // d5
 const byte txPin = 12; // d6
@@ -11,15 +19,23 @@ const byte txPin = 12; // d6
 // Set up a new SoftwareSerial object
 SoftwareSerial mySerial (rxPin, txPin);
 
+const byte wifiLED = 5; // d1
+
+unsigned long time_now = 0;
+int period = 1000; // time between blinks
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-    
+  pinMode(wifiLED, OUTPUT);
   // Set the baud rate for the SoftwareSerial object
   mySerial.begin(9600);
+  // prepare wifi
+   WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop() {
@@ -34,17 +50,32 @@ void loop() {
     String output = "output: " + message;
     Serial.println(output);
     lightBlink = false;
+    // this is where code should be uploaded. can it be done in a nonblocking way?
+    
   }
-  if (lightBlink == true) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(1000);  
-  }
-
-
+  blinkLight(lightBlink);
+  
 // the loop function runs over and over again forever
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    digitalWrite(wifiLED, HIGH);
+  } else {
+    digitalWrite(wifiLED, LOW);
+  }
+}
 
+// called from loop, just blinks the light.
+// 
+void blinkLight(bool keepBlinking) {
+  if (lightBlink == true) {
+    /*if (millis() > time_now + period) {
+      time_now = millis();
+      digitalWrite(LED_BUILTIN, lightOn);
+      lightOn = !lightOn; // switch the light
+      Serial.println(lightOn);
+   }  
+  }*/
+    digitalWrite(LED_BUILTIN, (millis() / period) % 2);
+  }
 }
 
 
